778e6f1b04f51c5b22f711490b7b0edf92642e22
omiswinner <mouseofsack911@gmail.com>
2024-01-30 18:01:36 +0900

    init

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+// az login でログインしておき、ログインするアカウントに kv の権限をつけておくこと
+// export KEY_VAULT_NAME="taktest" で環境変数を作っておくこと
+string keyVaultName = Environment.GetEnvironmentVariable("KEY_VAULT_NAME");
+var kvUri = "https://" + keyVaultName + ".vault.azure.net";
+
+var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
+
+// 取得したいシークレット名前
+string secretName = "taktest2";
+
+Console.WriteLine($"Retrieving your secret from {keyVaultName}.");
+var secret = await client.GetSecretAsync(secretName);
+Console.WriteLine($"Your secret is '{secret.Value.Value}'.");
---
+++ README.md
@@ -0,0 +1,4 @@
+
+- こちらを参照
+https://learn.microsoft.com/ja-jp/azure/key-vault/secrets/quick-create-net?tabs=azure-cli
+
